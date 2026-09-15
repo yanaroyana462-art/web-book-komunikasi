86e3ac4334515dec6934141c1cd2e06198e17c20
yanaroyana462-art <yanaroyana462@gmail.com>
2026-09-15 17:06:25 +0000

    fix: hapus kustom footer sementara

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Komunikasi Efektif ala Nabi',
-			components: {
-				Footer: './src/components/CustomFooter.astro',
-			},
+			
 			sidebar: [
 				{
 					label: 'Pendahuluan',
